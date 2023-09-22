2927e50fb7074e75b0795560abf5559059432367
Jacob Mark-Synard <145356032+JacobMarkSynard@users.noreply.github.com>
2023-09-22 08:27:43 -0400

    Fixed some grammar issues and added a comment

--- mad_libs.py
+++ mad_libs.py
@@ -1,15 +1,16 @@
 # Programmer: Jacob Mark-Synard
-# Description: The bot uses questions and makes a sentence.
+# Description: The bot uses questions to make a sentence
 
-# The bot asks the following questions to the user.
+# The bot asks questions to the user
 name = input ("What is your first name? ")
 book = input ("Give me your favorite book ")
 animal = input ("Give me an animal ")
 vehicle = input ("Give me a type of vehicle ")
 planet = input ("Tell me a planet. ")
 
+# Bot leaves as space between prompts 
 print()
 
-# Bot turns questons into a sentence.
+# Bot turns questons into a sentence
 print(f"{name} was reading {book}, the book that featured a {animal} ")
 print(f"flying a {vehicle} to the planet; {planet} for an adventure. ")
